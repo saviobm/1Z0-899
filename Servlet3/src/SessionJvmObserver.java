8e7110712d7917016c0f80f4d94fa37c742a849f
Sávio Bispo Maciel <savio.maciel@cnpq.br>
2017-11-13 13:43:38 -0200

    Sessões.

---
+++ Servlet3/src/SessionJvmObserver.java
@@ -0,0 +1,16 @@
+import javax.servlet.http.HttpSessionActivationListener;
+import javax.servlet.http.HttpSessionEvent;
+
+public class SessionJvmObserver implements HttpSessionActivationListener {
+
+	public void sessionDidActivate(HttpSessionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void sessionWillPassivate(HttpSessionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	} 
+
+}
